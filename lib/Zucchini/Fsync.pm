3f087ce380995c46c92abb953a80c7d396988f8e
Chisel Wright <chisel@herlpacker.co.uk>
2008-05-13 21:51:17 +0000

    r5086@wiggin:  chisel | 2008-05-13 20:31:59 +0100  / completed ttsite -> ::Fsync reimplementation  ! emty-dir-bug still exists

--- lib/Zucchini/Fsync.pm
+++ lib/Zucchini/Fsync.pm
@@ -7,11 +7,19 @@ use Zucchini::Version; our $VERSION = $Zucchini::VERSION;
 
 use Carp;
 use Config::Any;
+use Digest::MD5 qw(md5_hex);
+use File::Basename;
+use File::Find;
+use File::Slurp qw(read_file write_file);
+use File::Temp qw( tempfile );
 use Net::FTP;
+use Path::Class;
 
 # class data
-my %config_of       :ATTR( get => 'config',     set => 'config'     );
-my %ftpclient_of    :ATTR( get => 'ftp_client', set => 'ftp_client' );
+my %config_of       :ATTR( get => 'config',         set => 'config'         );
+my %ftpclient_of    :ATTR( get => 'ftp_client',     set => 'ftp_client'     );
+my %ftproot_of      :ATTR( get => 'ftp_root',       set => 'ftp_root'       );
+my %remotedigest_of :ATTR( get => 'remote_digest'   set => 'remote_digest'  );
 
 use Class::Std;
 {
@@ -36,14 +44,288 @@ use Class::Std;
         return;
     }
 
-    sub ftp_sync {
+    sub build_transfer_actions {
         my $self = shift;
-        my $config      = $self->get_config->get_siteconfig();
+        my $config  = $self->get_config->get_siteconfig();
+        my ($local_digest_file, $remote_digest_file);
+        my ($local_md5_of, $remote_md5_of, %transfer_action_of);
+
+        # the two files we are going to compare
+        $local_digest_file = file(
+            $config->{output_dir},
+            q{digest.md5}
+        );
+        warn "local: $local_digest_file\n";
+        $remote_digest_file = file(
+            $self->get_remote_digest
+        );
+        warn "remote: $remote_digest_file\n";
+
+        $local_md5_of   = $self->parse_md5file($local_digest_file);
+        $remote_md5_of  = $self->parse_md5file($remote_digest_file) || {};
+
+        # run through the list of files we have locally
+        foreach my $relpath (sort keys %{$local_md5_of}) {
+            my $dirname = dirname($relpath);
+
+            # does the file live in the server?
+            if (exists $remote_md5_of->{$relpath}) {
+                # if the MD5s match - nothing to do
+                if ($local_md5_of->{$relpath} eq $remote_md5_of->{$relpath}) {
+                    delete $local_md5_of->{$relpath};
+                    delete $remote_md5_of->{$relpath};
+                    next;
+                }
+
+                push @{$transfer_action_of{$dirname}},
+                {
+                    action  => 'update',
+                    relname => $relpath,
+                };
+                delete $local_md5_of->{$relpath};
+                delete $remote_md5_of->{$relpath};
+            }
+            # ... it's a new file to put on the server
+            else {
+                push @{$transfer_action_of{$dirname}},
+                {
+                    action  => 'new',
+                    relname => $relpath,
+                };
+                delete $local_md5_of->{$relpath};
+            }
+        }
+
+        # anything left in remote is a file we don't have locally
+        # we'll store actions (remove) for these, but won't act on the
+        # action until specifically asked
+        foreach my $relpath (sort keys %{$remote_md5_of}) {
+            my $dirname = dirname($relpath);
+            push @{$transfer_action_of{$dirname}},
+            {
+                action  => 'remove',
+                relname => $relpath,
+            };
+            delete $remote_md5_of->{$relpath};
+        }
+
+        # make sure we didn't miss anything
+        if (keys %{$local_md5_of}) {
+            warn qq{Some local files were not processed};
+            warn qq{Local:   } . pp($local_md5_of);
+        }
+        if (keys %{$remote_md5_of}) {
+            warn qq{Some remote files were not processed};
+            warn qq{Remote:   } . pp($remote_md5_of);
+        }
+
+        return \%transfer_action_of;
+    }
+
+    sub do_remote_update {
+        my $self                = shift;
+        my $transfer_actions    = shift;
+        my $config              = $self->get_config->get_siteconfig();
+        my $ftp                 = $self->get_ftp_client;
+        my $ftp_root            = $self->get_ftp_root;
+        my $errors              = 0;
+
+        if (not defined $ftp) {
+            warn(qq{No FTP server defined. Aborting upload.\n});
+            return;
+        }
+
+        # do transfer actions shortest dirname first
+        my @remote_dirs = sort {
+            length($a) <=> length($b)
+        } keys %{$transfer_actions};
+
+        my $ftp_root_status = $ftp->cwd($ftp_root);
+        if (not $ftp_root_status) {
+            die "$ftp_root: couldn't CWD to remote directory\n";
+        }
+        my $default_dir = $ftp->pwd();
+        if ($default_dir !~ m{/\z}xms) {
+            $default_dir .= q{/};
+        }
+
+        # make missing (remote) directories
+        warn "checking remote directories...\n"
+            if ($self->get_config->verbose(1));
+        foreach my $dir (@remote_dirs) {
+            my $status = $ftp->cwd($default_dir . $dir);
+            if (not $status) {
+                # verbose ouput
+                warn (q{MKDIR } . dir($default_dir, $dir) . qq{\n})
+                    if ($self->get_config->verbose(2));
+                # make the missing directory
+                $ftp->mkdir($default_dir . $dir)
+                    or warn qq{failed to create $dir};
+            }
+        }
+        # return to the default location
+        $ftp->cwd($default_dir);
+
+        # now run through everything and take the appropriate action for files
+        warn "transferring files...\n"
+            if ($self->get_config->verbose(1));
+        foreach my $dir (@remote_dirs) {
+            # run through the actions for the directory
+            foreach my $action ( @{$transfer_actions->{$dir}} ) {
+                if ($action->{action} =~ m{\A(?:new|update)\z}) {
+                    # verbose ouput
+                    warn (
+                        q{PUT }
+                        . $action->{relname}
+                        . q{ }
+                        . $action->{relname}
+                        . qq{\n}
+                    )
+                        if ($self->get_config->verbose(2));
+                    # send the file
+                    if (not $ftp->put( $action->{relname}, $action->{relname} )) {
+                        $errors++;
+                        warn "failed to upload $action->{relname}\n";
+                    }
+                }
+            }
+        }
+
+        # if we didn't have any errors, upload the digest
+        if (not $errors) {
+            $ftp->put('digest.md5');
+        }
+    }
+
+    sub fetch_remote_digest {
+        my $self = shift;
+        my $config  = $self->get_config->get_siteconfig();
+        my ($fh, $filename, $get_ok);
+
+        # a temporary file to use
+        ($fh, $filename) = tempfile();
+        $config->{tmp_remote_digest} = $filename;
+
+        # get the (remote) digest file
+        $get_ok = $self->get_ftp_client->get(
+            q{digest.md5},
+            $filename
+        );
+        if (not $get_ok) {
+            warn "No remote digest\n";
+            return;
+        }
+
+        $self->set_remote_digest($filename);
+
+        return;
+    }
+
+    sub ftp_sync {
+        my $self    = shift;
+        my $config  = $self->get_config->get_siteconfig();
+        my (@md5strings, $transfer_actions);
 
         # make sure we have an ftp client to use
         $self->prepare_ftp_client;
+        if (not defined $self->get_ftp_client) {
+            warn(qq{Failed to connect to remote FTP server. Aborting upload.\n});
+            return;
+        }
+
+        # regenerate (local) md5s
+        find(
+            sub{
+                $self->local_ftp_wanted(\@md5strings);
+            },
+            $config->{output_dir}
+        );
+        write_file(
+            qq{$config->{output_dir}/digest.md5},
+            @md5strings
+        );
+
+        # get the remote digest
+        $self->fetch_remote_digest;
+
+        # work out what needs to happen
+        $transfer_actions = $self->build_transfer_actions;
+
+        # do the remote update
+        $self->set_ftp_root(
+            $config->{'ftp'}{'path'} || '/'
+        );
+        $self->do_remote_update($transfer_actions);
 
         return;
+    }
+
+    sub local_ftp_wanted {
+        my ($self, $md5string_list) = @_;
+        my $config  = $self->get_config->get_siteconfig();
+
+        if (
+            -f $_
+                and
+            $_ ne q{digest.md5}
+                and
+            $_ !~ m{\.sw?}
+        ) {
+            push @{$md5string_list},
+                  $self->md5file($File::Find::name)
+                . qq{\n};
+        }
+    }
+
+    sub md5file {
+        my ($self, $file) = @_;
+        my $config  = $self->get_config->get_siteconfig();
+        my $dir_prefix = $config->{output_dir};
+        my ($filedata, $md5sum, $rel_filename, $md5data);
+
+        # slurp the file
+        $filedata = read_file($file)
+            or die "$file: $!";
+        # get the md5sum of the file
+        $md5sum = md5_hex($filedata);
+        # trim off any leading directories - making filename relative)
+        if (defined $dir_prefix) {
+            $rel_filename = $file;
+            $rel_filename =~ s{\A${dir_prefix}/}{};
+        }
+
+        # return an md5 string
+        return "$md5sum    $rel_filename";
+    }
+
+    sub parse_md5file {
+        my ($self, $file) = @_;
+        my (%md5_of, @lines);
+
+        if (not defined $file or $file =~ m{\A\s*\z}) {
+            carp "undefined filename passed to parse_md5file()";
+            return {};
+        }
+
+        if (! -f $file) {
+            carp "$file: file not found";
+            return {};
+        }
+
+        # read in the file - ".q{}" forces any Path::Class objects to be
+        # stringified
+        @lines = read_file($file.q{})
+            or die "$file: $!";
+
+        # parse/split each line
+        foreach my $line (@lines) {
+            chomp($line);
+            if ($line =~ m{\A([a-z0-9]{32})\s+(.+)\z}xms) {
+                $md5_of{$2} = $1;
+            }
+        }
+
+        return \%md5_of;
     }
 
     sub prepare_ftp_client {
