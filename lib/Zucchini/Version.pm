c31d10b94aecaeba7d881050fa7819feda5cc486
Chisel Wright <chisel@herlpacker.co.uk>
2009-03-06 22:34:11 +0000

    r9273@wiggin:  chisel | 2009-03-06 19:21:17 +0000  version bump to 0.0.15

--- lib/Zucchini/Version.pm
+++ lib/Zucchini/Version.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 # from mst on #catalyst
-use version; our $VERSION = qv(0.0.14)->numify;
+use version; our $VERSION = qv(0.0.15)->numify;
 
 package Zucchini::Version;
 
