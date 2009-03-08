99dcacdbeb50405dce40e88bc075fc27a71cc9b5
Chisel Wright <chisel@herlpacker.co.uk>
2009-03-08 10:58:51 +0000

    r9292@wiggin:  chisel | 2009-03-08 10:55:20 +0000  / version bump to 0.0.16  / checking in MANIFEST (should have changed this in 0.0.15 - oops)

--- lib/Zucchini/Version.pm
+++ lib/Zucchini/Version.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 # from mst on #catalyst
-use version; our $VERSION = qv(0.0.15)->numify;
+use version; our $VERSION = qv(0.0.16)->numify;
 
 package Zucchini::Version;
 
